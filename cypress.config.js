1e9629e0048b5e3cfb7d5831c940426a2e07838b
AnnieDroide <anitamercadojuncos@gmail.com>
2023-08-16 22:03:33 -0300

    Se agrega baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  env: {
-    viewportmobile: {
-      device: "iphone-xr",
-    },
-    viewportdesktop: {
-      device: "macbook"
-    }
-
-
-  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://www.edenentradas.com.ar/",
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
     },
   },
 });
