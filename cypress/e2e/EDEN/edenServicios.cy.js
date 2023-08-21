bcb0ae4664a8861690863c7c9da60287f0685b43
AnnieDroide <anitamercadojuncos@gmail.com>
2023-08-21 15:23:39 -0300

    Se agregan tests de servicio y carpeta de  Schemas

--- cypress/e2e/EDEN/edenServicios.cy.js
+++ cypress/e2e/EDEN/edenServicios.cy.js
@@ -1,11 +1,25 @@
 /// <reference types="cypress" />
 
 describe("TEST DE SERVICIO DE EDEN", () => {
-  it("Verificar Servicio de INICIO", () => {
-    cy.request(
-      "GET",
-      "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio"
-    ).then((respuesta) => {
+  it("Verificar Servicio de INICIO 1", () => {
+    cy.request({
+      method: "GET",
+      url: "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio",
+    }).then((respuesta) => {
+      cy.log(`Respuesta del servicio de Inicio: ${JSON.stringify(respuesta)}`);
+      expect(respuesta.status).to.eq(200);
+    });
+  });
+
+  it("Verificar Servicio de INICIO 2", () => {
+    cy.request({
+      method: "GET",
+      url: "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio",
+    }).then((respuesta) => {
+      cy.writeFile(
+        `cypress/fixtures/autogenerado/eventos.json`,
+        respuesta.body
+      );
       expect(respuesta.status).to.eq(200);
     });
   });
